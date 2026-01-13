7b4f75236bb82740332f0e2ced0305df07265ef2
Shinta Feronica Florence <shintaferonica99@gmail.com>
2026-01-13 22:58:18 +0700

    Update struktur folder dan tambah landing page

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+import HeroSection from "./components/home/hero";
+import CategoriesSection from "./components/home/categories";
+import ProductsSection from "./components/home/products";
+
+export default function Home() {
+  return <main>
+    <HeroSection/>
+    <CategoriesSection />
+    <ProductsSection />
+  </main>;
+}
